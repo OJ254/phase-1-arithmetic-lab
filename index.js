588694da13199b3b9cbadb361a40be9728bf97b1
Japheth Oruko <japhethoruko@gmail.com>
2024-09-26 09:04:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// Define num1 and num2 for multiplication
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2; // 62
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1; // Random integer greater than 0
+
+// Define num3 and num4 for modulus operation
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4; // 4
+
+// Find the highest number in a set
+const max = Math.max(1, 5, 10, 15, 20); // 20
